77001084151c75b32016d76136d26467e08d0043
Jessie Zhang <jessiezhang98@gmail.com>
2021-03-22 14:58:43 +0000

    arduino stream

--- poetry_generator/routes.py
+++ poetry_generator/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, current_app
 from .generator import ai
 from . import socketio
+import serial
 
 generator = Blueprint('generator', __name__)
+arduino = serial.Serial('/dev/tty.usbmodem14201', 9600)
 
 from time import sleep
 from random import random
@@ -12,15 +14,16 @@ from threading import Thread, Event
 thread = Thread()
 thread_stop_event = Event()
 
-def randomNumberGenerator():
-    print("Making random numbers")
+def readArduino():
+    print("Reading Arduino serial")
     while not thread_stop_event.isSet():
-        number = round(random()*10, 3)
-        print(number)
-        socketio.emit('newnumber', {'number': number}, namespace='/test')
-        socketio.sleep(5)
+        datastream = arduino.readline()
+        decoded_proximity = float(datastream[0:len(datastream)-2].decode("utf-8"))
+        print(decoded_proximity)
+        socketio.emit('arduinoread', {'number': decoded_proximity}, namespace='/arduino')
+        socketio.sleep(1)
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/arduino')
 def test_connect():
     # need visibility of the global thread object
     global thread
@@ -29,13 +32,8 @@ def test_connect():
     #Start the random number generator thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
-        thread = socketio.start_background_task(randomNumberGenerator)
+        thread = socketio.start_background_task(readArduino)
 
 @generator.route('/')
 def index():
     return render_template('index.html')
-
-@generator.route('/poem', methods=['POST'])
-def analyse():
-    text = "hello"
-    return render_template('index.html', text=text)
--- poetry_generator/static/js/app.js
+++ poetry_generator/static/js/app.js
@@ -1,11 +1,11 @@
 window.onload=function(){
     //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/arduino');
 
     //receive details from server
-    socket.on('newnumber', function(msg) {
-        console.log("Received number" + msg.number);
-        document.getElementById("elem").innerHTML = msg.number;
+    socket.on('arduinoread', data => {
+        console.log("Proximity value: " + data.number);
+        document.getElementById("arduino").innerHTML = data.number;
     });
 
 };
